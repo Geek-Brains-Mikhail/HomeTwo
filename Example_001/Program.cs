3f79fbad76c1d892a30ae5159da791adfec8ec83
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-10 01:41:25 +0300

    Ex_001

---
+++ Example_001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+//Code//
+int WriteNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+int NumberMiddle(int number)
+{
+    int result = ((number/100) * 100) +  (number%10);
+    return (number-result) /10;
+
+}
+
+Console.WriteLine(NumberMiddle(WriteNumber("Введите трехзначеное число: ")));
