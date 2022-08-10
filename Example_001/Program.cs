bd3f81fadeca34e5a8f90470dfdb6701e03f468a
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-10 17:20:40 +0300

    NewNewEx_001

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -9,13 +9,20 @@ int WriteNumber(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
-
 }
 int NumberMiddle(int number)
 {
     int result = ((number/100) * 100) +  (number%10);
     return (number-result) /10;
-
 }
 //************Code************//
-Console.WriteLine(NumberMiddle(WriteNumber("Введите трехзначеное число: ")));
+Console.Clear();
+int number = WriteNumber("Введите трехзначное число: ");
+if(number < 100 || number > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число!(");
+}
+else
+{
+    Console.WriteLine(NumberMiddle(number));
+}
